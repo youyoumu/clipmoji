ec6b62210ecce87a64becf7b08f3ccd0fa63d148
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-06-29 17:17:02 +0700

    Display video

--- src/routes/-components/GifCard.tsx
+++ src/routes/-components/GifCard.tsx
@@ -1,10 +1,10 @@
 import { Card, CardBody, CardHeader, Image } from "@heroui/react";
+import ReactPlayer from "react-player";
 
 import { useCachedBlob } from "#/hooks/useCachedBlob";
 import type { FavGif } from "#/lib/db";
 
 export default function GifCard({ favGif }: { favGif: FavGif }) {
-  if (favGif.type === "mp4") return null;
   const { data: blob } = useCachedBlob(favGif.src);
   if (!blob) return null;
   const blobUrl = URL.createObjectURL(blob);
@@ -16,15 +16,35 @@ export default function GifCard({ favGif }: { favGif: FavGif }) {
         <h4 className="font-bold text-large">Frontend Radio</h4>
       </CardHeader>
       <CardBody className="overflow-visible py-2">
-        <Image
-          alt="Card background"
-          className="object-cover rounded-xl"
-          src={blobUrl}
-          width="full"
-          classNames={{
-            img: "w-full",
-          }}
-        />
+        {(() => {
+          if (blob.type === "image/gif") {
+            return (
+              <Image
+                alt="Card background"
+                className="object-cover rounded-xl"
+                src={blobUrl}
+                width="full"
+                classNames={{
+                  img: "w-full",
+                }}
+              />
+            );
+          }
+          if (blob.type === "video/mp4") {
+            return (
+              <ReactPlayer
+                src={blobUrl}
+                playing
+                loop
+                playsInline
+                muted
+                controls={false}
+                width="100%"
+                height="100%"
+              />
+            );
+          }
+        })()}
       </CardBody>
     </Card>
   );
