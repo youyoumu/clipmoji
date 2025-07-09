825736d9d0bfea3c22d21782b35a314adf587903
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-07-09 22:28:47 +0700

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,25 @@
 import { resolve } from "node:path";
 
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-  },
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    viteReact(),
+  ],
   resolve: {
     alias: {
       "#": resolve(__dirname, "./src"),
+
+      // https://github.com/tabler/tabler-icons/issues/1233#issuecomment-2428245119
+      // /esm/icons/index.mjs exports the icons statically, so no separate chunks are created
+      "@tabler/icons-react": "@tabler/icons-react/dist/esm/icons/index.mjs",
     },
   },
 });
